f1a755e545f42e5d459ee0ba1ecc160cfcda0823
Alkamist <coreylehman@me.com>
2023-12-24 10:32:40 -0500

    Seeing if this fixes the theme toggle hydration problem.

--- app/themeSwitch.tsx
+++ app/themeSwitch.tsx
@@ -1,10 +1,20 @@
+import { useState, useEffect } from 'react'
 import { useTheme } from 'next-themes'
 
 const ThemeSwitch = ({ spacingOnly }: any) => {
+  const [mounted, setMounted] = useState(false)
   const { theme, setTheme } = useTheme()
 
   const toggleTheme = () => {
     theme === "dark" ? setTheme("light") : setTheme("dark")
+  }
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) {
+    return null
   }
 
   return (
